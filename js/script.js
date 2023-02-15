d545e3e9a79ba9dab81635a3404270bd2ea3e628
successhwan <44success.sh@gmail.com>
2023-02-15 16:13:17 +0900

    back

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-window.onfocus=function(){
-}
-window.onload=function(){
- window.focus(); // 현재 window 즉 익스플러러를 윈도우 최상단에 위치
-window.moveTo(0,0); // 웹 페이지의 창 위치를 0,0 (왼쪽 최상단) 으로 고정
-window.resizeTo(1600,800); // 웹페이지의 크기를 가로 1280 , 세로 800 으로 고정(확장 및 축소)
-}
 $("document").ready(function () {
   // 문서의 dom이 준비되면
   // .mySwiper 클래스를 swiper 슬라이더로 만듦
